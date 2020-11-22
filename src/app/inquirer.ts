1a4676ccdec5a125cffb45b742350f9492e8ee4b
Sake <pan.ziqiang@holytiny.com>
2020-11-23 00:42:46 +0800

    feat: read config yaml

--- src/app/inquirer.ts
+++ src/app/inquirer.ts
@@ -1,7 +1,34 @@
-export class Inquirer {
-  async run(argv: Array<string>): Promise<number> {
-    console.log(argv);
+import * as fs from 'fs'
+import * as YAML from 'yaml'
 
-    return 0;
+import { ReturnCode } from '../common/return-code';
+export class Inquirer {
+  constructor(
+    private  configFilePath = './inquirer.yaml'
+  ) {
+      
+  }
+
+  async run() {
+    const yaml = this.yaml();
+
+    this.exit('inquirer succeed!', ReturnCode.OK);
+  }
+
+  exit(message: string, code: number) {
+    console.info(message);
+    process.exit(code);
+  }
+
+  yaml() {
+    try {
+      // console.log(process.cwd());
+      const inputFile = fs.readFileSync(this.configFilePath, 'utf8');
+      const yaml = YAML.parse(inputFile);
+
+      return yaml;
+    } catch (e) {
+      this.exit(e.message, ReturnCode.ReadConfigFileError);
+    }    
   }
 }
